b4a4798a1a601006255d08e859ede6d905a5a3bb
foreverdyz <44935367+foreverdyz@users.noreply.github.com>
2022-09-12 19:06:43 -0400

    Create leetcode392.py

---
+++ leetcode392.py
@@ -0,0 +1,10 @@
+def method1(s: str, t: str) -> bool:
+  if len(s) == 0:
+     return True
+  pivot = 0
+  for x in t:
+    if x == s[pivot]:
+    pivot += 1
+    if pivot >= len(s):
+      return True
+  return False
